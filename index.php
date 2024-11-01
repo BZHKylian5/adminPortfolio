daecc9483f8a683d07c11a5db9133b04f43950fa
BZHKylian <kylian.houedec.56@gmail.com>
2024-11-01 17:22:07 +0100

    ajout du link dans index car oublier de le mettre pour voir les changement

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once "config.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="style.css"> <!-- Lien vers le fichier CSS -->
+    <link rel="stylesheet" href="css/style.css"> <!-- Lien vers le fichier CSS -->
 </head>
 <body>
     <header>    
