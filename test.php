ac3609d5b94bd6191cf019ccdaa7f297e9cdb3f5
BZHKylian <kylian.houedec.56@gmail.com>
2024-11-01 20:06:36 +0100

    test

---
+++ test.php
@@ -0,0 +1,32 @@
+<?php
+require_once "config.php";
+
+// Vérifie si l'utilisateur est connecté
+if (!isset($_SESSION['idUser'])) {
+    header("Location: login.php"); // Redirige vers la page de connexion
+    exit(); // Assure que le script s'arrête après la redirection
+}
+
+$idUser = $_SESSION['idUser']; // Assurez-vous que vous récupérez l'ID utilisateur de la session
+
+$stmt = $conn->prepare("SELECT * from utilisateur WHERE id_utilisateur = ?");
+$stmt->bind_param("i", $idUser);
+$stmt->execute();
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
+
+$stmt = $conn->prepare("SELECT *
+                         FROM utilisateur u
+                         LEFT JOIN photo_profil pp ON u.id_utilisateur = pp.id_utilisateur
+                         WHERE u.id_utilisateur = ?");
+if ($stmt === false) {
+    die("Erreur de préparation de la requête : " . $conn->error); // Affiche l'erreur si la préparation échoue
+}
+
+// Lier le paramètre
+$stmt->bind_param("i", $idUser);
+$stmt->execute();
+$result = $stmt->get_result();
+$profile = $result->fetch_assoc();
+print_r($profile)
+?>
