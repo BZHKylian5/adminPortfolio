64a1450d04bcdebacfad6ee0e084051a303034a3
BZHKylian <kylian.houedec.56@gmail.com>
2024-11-02 00:06:17 +0100

    test

--- db.php
+++ db.php
@@ -8,4 +8,6 @@ $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$conn->set_charset("utf8mb4");
 ?>
